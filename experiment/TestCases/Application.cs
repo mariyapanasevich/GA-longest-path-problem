6fcdf6d565a8ea05d8feab192951e2aaa76f9b65
Mariya Panasevich <panasevich.mariya@gmail.com>
2017-08-20 20:40:47 +0300

    Add files via upload

--- experiment/TestCases/Application.cs
+++ experiment/TestCases/Application.cs
@@ -11,103 +11,106 @@ using System.Text.RegularExpressions;
 using OpenQA.Selenium.Support.UI;
 using System.Threading;
 using ExperimentForLPP.Classes;
- 
+
 namespace ExperimentForLPP
 {
     class Application
     {
+        GA obj;
+
         private IWebDriver driver;
 
         [FindsBy(How = How.Id, Using = "textBox1")]
-        public IWebElement numVertex {get; set;}
+        public IWebElement numVertex { get; set; }
 
         [FindsBy(How = How.Id, Using = "textBox2")]
         public IWebElement numEdge { get; set; }
 
         [FindsBy(How = How.Id, Using = "textBox3")]
         public IWebElement SizePop { get; set; }
-        
+
         [FindsBy(How = How.Id, Using = "textBox4")]
         public IWebElement persentCrossover { get; set; }
 
         [FindsBy(How = How.Id, Using = "textBox5")]
         public IWebElement numStep { get; set; }
-        
+
         [FindsBy(How = How.Id, Using = "textBox7")]
         public IWebElement firstVertex { get; set; }
-        
+
         [FindsBy(How = How.Id, Using = "textBox6")]
         public IWebElement secondVertex { get; set; }
-        
+
         [FindsBy(How = How.Id, Using = "checkBox1")]
         public IWebElement AllPath { get; set; }
-       
+
         [FindsBy(How = How.Id, Using = "checkBox2")]
         public IWebElement betweenVertex { get; set; }
 
         [FindsBy(How = How.Id, Using = "radioButton1")]
         public IWebElement nonintersectingpaths { get; set; }
-        
-      [FindsBy(How = How.Id, Using = "radioButton2")]
+
+        [FindsBy(How = How.Id, Using = "radioButton2")]
         public IWebElement intersectingpaths { get; set; }
 
         [FindsBy(How = How.Id, Using = "radioButton3")]
         public IWebElement mutate { get; set; }
-       
+
         [FindsBy(How = How.Id, Using = "radioButton4")]
         public IWebElement noninterAndinterpaths { get; set; }
-        
-        [FindsBy(How = How.Id, Using = "listBox1")]
-        public IWebElement result { get; set; }
-        
+
         [FindsBy(How = How.Id, Using = "button1")]
         public IWebElement start { get; set; }
 
+        [FindsBy(How = How.Id, Using = "button2")]
+        public IWebElement clear { get; set; }
+
+        [FindsBy(How = How.Id, Using = "dataGridView1")]
+        public IWebElement dataGrid { get; set; }
+        private int num;
         public Application(IWebDriver driver)
         {
             this.driver = driver;
             PageFactory.InitElements(driver, this);
+            num = 0;
         }
 
-        public void findlongestPath(int type, int time)
+        public void findlongestPath(int type)
         {
-           
+            numVertex.SendKeys(obj.numVertex.ToString());
+            numEdge.SendKeys(obj.numEdge.ToString());
+            List<List<int>> dataTable = stringToList(obj.graph, obj.numEdge);
+            setValue(dataTable);
+            SizePop.SendKeys(obj.PopulationSize.ToString());
 
             try
             {
+
                 if (type == 1)
                 {
-                    GaInAllPath data = ExcelDataAccess.GetTestData<GaInAllPath>(type);
-                    numVertex.SendKeys(data.numVertex.ToString());
-                    numEdge.SendKeys(data.numEdge.ToString());
-                    SizePop.SendKeys(data.PopulationSize.ToString());
-                    List<List<int>> dataTable = stringToList(data.graph, data.numEdge);
-                    setValue(dataTable);
-                   
+                    var data = (GaInAllPath)obj;
+                  
                     AllPath.Click();
                     data.setTypeAlgorithm();
 
-                    if (data.typeAlgorithm == 1)
+                    switch(data.typeAlgorithm)
                     {
+                        case 1:    
                         nonintersectingpaths.Click();
-                    }
-                    else if (data.typeAlgorithm == 2)
-                    {
+                        break;
+                        case 2:
                         intersectingpaths.Click();
-                    }
-                    else if (data.typeAlgorithm == 3)
-                    {
+                        break;
+                        case 3:
                         mutate.Click();
-                    }
-                    else if (data.typeAlgorithm == 4)
-                    {
+                        break;
+                        case 4:
                         noninterAndinterpaths.Click();
-                    }
-                    else
-                    {
+                        break;
+                        default:
                         throw new Exception("Algorithm didn't find!");
+                        break;
                     }
-
                     persentCrossover.SendKeys(data.persentToCross.ToString());
                     if (data.typeAlgorithm == 3 || data.typeAlgorithm == 4)
                     {
@@ -116,12 +119,7 @@ namespace ExperimentForLPP
                 }
                 else if (type == 2)
                 {
-                    GaBetweenVertex data = ExcelDataAccess.GetTestData<GaBetweenVertex>(type);
-                    numVertex.SendKeys(data.numVertex.ToString());
-                    numEdge.SendKeys(data.numEdge.ToString());
-                    SizePop.SendKeys(data.PopulationSize.ToString());
-                    List<List<int>> dataTable = stringToList(data.graph, data.numEdge);
-                    setValue(dataTable);
+                    var data = (GaBetweenVertex)obj;
                     betweenVertex.Click();
                     numStep.SendKeys(data.numberStep.ToString());
                     firstVertex.SendKeys(data.firstVertex.ToString());
@@ -137,22 +135,6 @@ namespace ExperimentForLPP
                 Console.WriteLine("Error:" + ex.Message);
             }
 
-            start.Click();
-            Thread.Sleep(time*1000);
-           
-        }
-
-        public  void Click(IWebElement element, int timeout)
-        {
-            var wait = new DefaultWait<IWebElement>(element);
-            wait.IgnoreExceptionTypes(typeof(WebDriverException));
-            wait.PollingInterval = TimeSpan.FromMilliseconds(10);
-            wait.Timeout = TimeSpan.FromSeconds(timeout);
-            wait.Until<bool>(drv =>
-            {
-                element.Click();
-                return true;
-            });
         }
 
         public void setValue(List<List<int>> element)
@@ -162,16 +144,16 @@ namespace ExperimentForLPP
                 string tmp = "Строка " + i.ToString();
 
                 List<IWebElement> obj = driver.FindElement(By.Name(tmp)).FindElements(By.Name(" " + tmp)).ToList();
-                for(int j=0; j < obj.Count(); j++ )
+                for (int j = 0; j < obj.Count(); j++)
                 {
                     obj[j].SendKeys(element[i][j].ToString());
-                }   
+                }
             }
         }
 
         public List<List<int>> stringToList(string value, int numEdge)
         {
-            List<string> tmp1=  value.Split( new Char[]{'\n'}).ToList();
+            List<string> tmp1 = value.Split(new Char[] { '\n' }).ToList();
 
             List<List<int>> result = new List<List<int>>();
             for (int i = 0; i < tmp1.Count(); i++)
@@ -180,5 +162,30 @@ namespace ExperimentForLPP
             }
             return result;
         }
+
+
+        public void cycleStart(int type, int time, int cycle)
+        {
+            if (type == 1)
+            {
+                obj = ExcelDataAccess.GetTestData<GaInAllPath>(type);
+            }
+            else if (type == 2)
+            {
+                obj = ExcelDataAccess.GetTestData<GaBetweenVertex>(type);
+            }
+
+            findlongestPath(type);
+            do
+            {
+                start.Click();
+                Thread.Sleep(time*1000);
+                num++;
+            }
+            while (cycle != num);
+
+            driver.Close();
+        }
+
     }
 }
