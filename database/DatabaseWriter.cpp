6e0c713bb2f0626abe730f90e2030e1ff752cde9
Mariya Panasevich <panasevich.mariya@gmail.com>
2017-08-27 23:52:12 +0300

    Add files via upload

---
+++ database/DatabaseWriter.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include "libpq-fe.h"
+#include "DatabaseWriter.h"
+#include <windows.h>
+#include <iostream>
+#include <msclr\marshal_cppstd.h>
+
+
+using namespace System::Windows::Forms;
+using namespace std;
+using namespace System;
+using namespace msclr::interop;
+
+const char* CONNECTIONSTRING = "hostaddr = '127.0.0.1' port = '5433' dbname = 'GA' user = 'postgres' password = 'ermolaeva7' connect_timeout = '0'";
+
+void DatabaseWriter::CloseConn(PGconn *conn)
+{
+	PQfinish(conn);
+	getchar();
+
+}
+
+PGconn* DatabaseWriter::ConnectDB()
+{
+	conn = NULL;
+	statusConnect = true;
+
+	conn = PQconnectdb(CONNECTIONSTRING);
+
+	if (PQstatus(conn) != CONNECTION_OK)
+	{
+		statusConnect = false;
+		CloseConn(conn);
+	}
+
+	return conn;
+}
+
+void DatabaseWriter::error(string mess)
+{
+	String^ res1 = marshal_as<String^>(mess);
+	MessageBox::Show(res1 , "Error", MessageBoxButtons::OK, MessageBoxIcon::Exclamation);	
+	exit(1);
+}
---
+++ database/DatabaseWriter.h
@@ -0,0 +1,88 @@
+#pragma once
+#include <libpq-fe.h>
+#include <string>
+#include "GA.h"
+#include <typeinfo>
+#include <windows.h>
+#include "Problem.h"
+#include "GaInAllPath.h"
+#include <vector>
+#include "GaBetweenVertex.h"
+
+using namespace std;
+
+class DatabaseWriter
+{
+public:
+	void CloseConn(PGconn *conn);
+	PGconn* ConnectDB();
+	bool statusConnect;
+	PGresult *res;
+	PGconn *conn;
+
+	template <class T> PGresult* insertData(int typeTask, T elem, Problem G, int nParamCount, ...)
+	{
+		string command = "";
+		if (typeTask == 1)
+		{
+			GaInAllPath  data = dynamic_cast<GaInAllPath&>(elem);
+			string result = "'" + vector_to_string(data.res_population)+"'";
+			if (data.type == 1 || data.type == 2)
+			{
+				command = "INSERT INTO public.gainallgraph( id_task, vertex, edge, \"time\", sizenewpopulation, path, lengthpath, id_algorithm, persent) VALUES( 1 ," + to_string(G.n) + " , " +
+					to_string(G.m) + " , " + to_string(data.time1) + " , " + to_string(data.populationSize) + " , " +
+					result + " , " + to_string(data.res_population[0].size() - 1) + " , " + to_string(data.type) + " , " +
+					to_string(data.percent * 100) + ")";
+
+			}
+			else if (data.type == 3 || data.type == 4)
+			{
+			
+				va_list ap;
+				va_start(ap, nParamCount);
+				int n = va_arg(ap, int);
+				va_end(ap);
+				command = "INSERT INTO public.gainallgraphwithstep( id_task, vertex, edge, \"time\", sizenewpopulation, path, lengthpath, id_algorithm, persent,  numstep) VALUES( 1 ," + to_string(G.n) + " , " +
+					to_string(G.m) + " , " + to_string(data.time1) + " , " + to_string(data.populationSize) + " , " +
+					result + " , " + to_string(data.res_population[0].size() - 1) + " , " + to_string(data.type) + " , " +
+					to_string(data.percent*100) + " , " + to_string(n) + ")";
+			}
+		}
+		else if (typeTask == 2)
+		{
+			GaBetweenVertex  data = dynamic_cast<GaBetweenVertex&>(elem);
+			string result = "'" + vector_to_string(data.res_population) + "'";
+			va_list ap;
+			va_start(ap, nParamCount);
+			int n = va_arg(ap, int);
+			va_end(ap);
+
+			command = "INSERT INTO public.gabetweenvertex( id_task, vertex, edge, \"time\", sizenewpopulation, path, lengthpath, firstvertex, secondvertex , numstep) VALUES( 2 ," + to_string(G.n) + " , " +
+				to_string(G.m) + " , " + to_string(data.time1) + " , " + to_string(data.sizeStartPopulation) + " , " +
+				result + " , " + to_string(data.res_population[0].size() - 1) + " , "  +
+				to_string(data.s) + " , " + to_string(data.t) + " , " + to_string(n)+")";
+		}
+		res = PQexec(conn, command.c_str());
+		if (PQresultStatus(res) != PGRES_COMMAND_OK)
+		error(PQresultErrorMessage(res));
+		return res;	
+	}
+
+	void error(string error);
+	DatabaseWriter(){};
+
+	string vector_to_string(vector<vector<long long>>vec)
+	{
+		string res = "";
+
+		for (size_t i = 0; i < vec.size(); i++)
+		{
+			for (size_t j = 0; j < vec[i].size() - 1; j++)
+			{
+				res = res + "(" + to_string(vec[i][j]) + "," + to_string(vec[i][j + 1]) + ")";
+			}
+			if (i != vec.size() - 1) res = res + " ; ";
+		}
+		return res;
+	}
+};
